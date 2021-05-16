b9126c45d126457ea7761810ff18e51974f867da
Matheus Mello de Lima <mellomatheuslima@gmail.com>
2021-05-16 16:10:38 -0300

    Add cringe command as a class consulting the cringe words database

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const { Telegraf } = require('telegraf');
 const AddCringeWordCommand = require('./commands/AddCringeWord.command');
 const { calculateCringePoints, rateCringe } = require('./commands/cringera');
+const CringeraCommand = require('./commands/Cringera.command');
 require('dotenv').config();
 
 // Todo: Ranking de Cringe
@@ -10,20 +11,8 @@ bot.start((ctx) => ctx.reply('Fala aí! Vejo que você também é um Lord do Cri
 
 bot.command('/cringera', ctx => {
   console.log('Bot received /cringera command request');
-  if (!ctx.message.reply_to_message) {
-    console.log("Rejecting message because there isn't a 'reply to' message");
-    ctx.reply('Você precisa mencionar a mensagem para passar pela perícia do Cringe.', { reply_to_message_id: ctx.message.message_id });
-    return;
-  }
-
-  const message = ctx.message.reply_to_message;
-  const text = message.text;
-  console.log(`Message: ${text}`);
-  const cringePoints = calculateCringePoints(text, cringeWords);
-  console.log(`Cringe points: ${cringePoints}`);
-  const cringeRating = rateCringe(cringePoints);
-  console.log(`Cringe rating: ${cringeRating}`);
-  ctx.reply(cringeRating, { reply_to_message_id: message.message_id });
+  const command = new CringeraCommand();
+  await command.handle(ctx);
 });
 
 bot.command('/addcringeword', async ctx => {
---
+++ src/commands/Cringera.command.js
@@ -0,0 +1,59 @@
+const CringeWord = require('../models/CringeWord.model');
+
+class CringeraCommand {
+
+  async handle(ctx) {
+    if (!ctx.message.reply_to_message) {
+      console.log("Rejecting message because there isn't a 'reply to' message");
+      ctx.reply('Você precisa mencionar a mensagem para passar pela perícia do Cringe.', { reply_to_message_id: ctx.message.message_id });
+      return;
+    }
+
+    const message = ctx.message.reply_to_message;
+    const text = message.text;
+    console.log(`Message: ${text}`);
+    const cringePoints = await this.calculateCringePoints(text);
+    console.log(`Cringe points: ${cringePoints}`);
+    const cringeRating = this.rateCringe(cringePoints);
+    console.log(`Cringe rating: ${cringeRating}`);
+    ctx.reply(cringeRating, { reply_to_message_id: message.message_id });
+  }
+
+  async calculateCringePoints(message) {
+    const msg = message.toLowerCase();
+    let cringePoints = 0;
+
+    const cringeWords = await CringeWord.findAll();
+
+    cringeWords.forEach(cw => {
+      if (msg.includes(cw)) {
+        cringePoints++;
+      }
+    });
+
+    return cringePoints;
+  }
+
+  rateCringe(cringePoints) {
+    if (cringePoints === 0) {
+      return 'Normie.';
+    }
+
+    if (cringePoints < 4) {
+      return 'Cringe padrão, nada demais. Todo mundo solta um cringe uma hora ou outra.';
+    }
+
+    if (cringePoints < 7) {
+      return 'Linha tênue do cringe. Você precisa se atentar.';
+    }
+
+    if (cringePoints < 10) {
+      return 'Cringe++. Parece chat de jogador high elo de TFT.';
+    }
+
+    return 'Enquete do Cringe.';
+  }
+
+}
+
+module.exports = CringeraCommand;
