fc7f24b876dc5e6896691e6fc4b78bd7fb88a2a7
Matheus Mello de Lima <mellomatheuslima@gmail.com>
2021-05-16 16:24:53 -0300

    Add default value for database uri

--- src/database.js
+++ src/database.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize');
 
-const database = new Sequelize(process.env.DATABASE_URI, {
+const database = new Sequelize(process.env.DATABASE_URI || 'sqlite::memory:', {
   define: {
     freezeTableName: true,
   },
