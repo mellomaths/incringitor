be94907578817e58869e7e28d2195a55853fa0ea
Matheus Mello de Lima <mellomatheuslima@gmail.com>
2021-06-27 07:33:09 -0300

    Add types for chat message

--- src/abstract/BotContext.ts
+++ src/abstract/BotContext.ts
@@ -1,25 +1,31 @@
 import { Context } from 'telegraf';
-import { Chat, Message, Update, User } from "typegram";
+import { Update } from "typegram";
 
-export class ChatMessage implements Message.ServiceMessage, Message.TextMessage {
-  message_id: number;
-  from?: User;
-  sender_chat?: Chat;
-  date: number;
-  chat: Chat;
+export class ChatUser {
+  id: number;
+  is_bot: false;
+  first_name: string;
+  last_name: string;
+  username: string;
+}
 
+export class ChatMessage {
+  id: number;
+  from?: ChatUser;
+  date: Date;
   text: string;
 
   constructor(message: any) {
-    const keys = Object.keys(message) // get items keys
-    const thisKeys = Object.keys(this) // get this class keys
-    const limitedItem = keys.reduce((newObj, key) => { // combine same keys
-      if (thisKeys.includes(key)) {
-        newObj[key] = message[key]
-      }
-      return newObj
-    }, {})
-    Object.assign(this, limitedItem) // assign to this class
+    this.id = message.message_id;
+    this.from = {
+      id: message.from.id,
+      is_bot: message.from.is_bot,
+      first_name: message.from.first_name,
+      last_name: message.from.last_name,
+      username: message.from.username,
+    };
+    this.date = new Date(message.date);
+    this.text = message.text;
   }
   
   cleanup(commandCallout: string): string {
