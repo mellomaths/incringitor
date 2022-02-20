a9479f7761219f4e316162090a46d96ed1e9ddb1
Matheus Mello de Lima <mellomatheuslima@gmail.com>
2022-02-19 22:26:11 -0300

    Add TaskSchedulerCommand that sends messages on a specific frequency

---
+++ src/commands/TaskScheduler.command.ts
@@ -0,0 +1,56 @@
+import * as cron from "node-cron";
+import { BotContext } from "../abstract/BotContext";
+import { Command } from "./Command";
+
+enum TaskScheduleTypes {
+  DAILY = "daily",
+}
+
+class TaskSchedulerConfig {
+  public static readonly DAILY = "0 12 * * *";
+}
+
+export class TaskSchedulerCommand extends Command {
+  constructor() {
+    super("/schedule");
+  }
+
+  async handle(context: BotContext): Promise<void> {
+    super.handle(context);
+    const sentence = context.chatMessage.cleanup(this.callout);
+    console.log(
+      `TaskSchedulerCommand.handle: Handling task scheduler for ${sentence}`
+    );
+    const words = sentence.split(" ");
+    if (words.length === 0) {
+      console.log(
+        `TaskSchedulerCommand.handle: Rejecting message due to schedule task without type`
+      );
+      context.reply("Informe em qual frequência o bot deve enviar a mensagem");
+      return;
+    }
+
+    const taskType = words.shift();
+    const message = words.join(" ");
+    console.log(
+      `TaskSchedulerCommand.handle: Scheduling to send message ${message} on the frequency of ${taskType}`
+    );
+
+    if (taskType === TaskScheduleTypes.DAILY) {
+      cron.schedule(TaskSchedulerConfig.DAILY, function () {
+        console.log(
+          `TaskSchedulerCommand.handle: Sending scheduled message ${message}`
+        );
+        context.reply(message);
+      });
+    } else {
+      console.log(
+        `TaskSchedulerCommand.handle: Rejecting message due to schedule task with invalid type`
+      );
+      context.reply(
+        `Informe em qual frequência o bot deve enviar a mensagem. Aqui está as possíveis frequências que o bot permite: ${TaskScheduleTypes}`
+      );
+      return;
+    }
+  }
+}
--- src/config/CommandLoader.ts
+++ src/config/CommandLoader.ts
@@ -1,17 +1,18 @@
-import { AddCringeWordCommand } from '../commands/AddCringeWord.command';
-import { Command } from '../commands/Command';
-import { CringeraCommand } from '../commands/Cringera.command';
-import { DaysInTheYearCommand } from '../commands/DaysInTheYear.command';
-import { DaysOfBolsonaro } from '../commands/DaysOfBolsonaro.command';
+import { AddCringeWordCommand } from "../commands/AddCringeWord.command";
+import { Command } from "../commands/Command";
+import { CringeraCommand } from "../commands/Cringera.command";
+import { DaysInTheYearCommand } from "../commands/DaysInTheYear.command";
+import { DaysOfBolsonaro } from "../commands/DaysOfBolsonaro.command";
+import { TaskSchedulerCommand } from "../commands/TaskScheduler.command";
 
 export class CommandLoader {
-
   static load(): Array<Command> {
     const commands: Array<Command> = [];
     commands.push(new AddCringeWordCommand());
     commands.push(new CringeraCommand());
     commands.push(new DaysOfBolsonaro());
     commands.push(new DaysInTheYearCommand());
+    commands.push(new TaskSchedulerCommand());
     return commands;
   }
 }
