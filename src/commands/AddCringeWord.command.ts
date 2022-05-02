104c3d8953cd7690a21b1fe2115c886c65b659e3
Matheus Mello de Lima <mellomatheuslima@gmail.com>
2022-05-02 10:46:54 -0300

    Rejects empty cringe words

--- src/commands/AddCringeWord.command.ts
+++ src/commands/AddCringeWord.command.ts
@@ -1,19 +1,29 @@
-import { BotContext } from '../abstract/BotContext';
-import { CringeWordRepository } from '../repository/CringeWord.repository';
-import { Command } from './Command';
+import { BotContext } from "../abstract/BotContext";
+import { CringeWordRepository } from "../repository/CringeWord.repository";
+import { Command } from "./Command";
 
 export class AddCringeWordCommand extends Command {
-
   private readonly repository = new CringeWordRepository();
 
   constructor() {
-    super('/addcringeword');
+    super("/addcringeword");
   }
 
   async handle(context: BotContext): Promise<void> {
     super.handle(context);
     const sentence = context.chatMessage.cleanup(this.callout);
-    console.log(`AddCringeWordCommand.handle: Saving '${sentence}' as a cringe word.`);
+    if (!sentence) {
+      console.log(
+        `AddCringeWordCommand.handle: Rejecting empty cringe word (sentence=${sentence}).`
+      );
+      context.reply(
+        `Informe qual palavra ou frase deseja adicionar ao repositório de palavras cringes.`
+      );
+      return;
+    }
+    console.log(
+      `AddCringeWordCommand.handle: Saving (sentence=${sentence}) as a cringe word.`
+    );
     await this.repository.save(sentence);
   }
 }
